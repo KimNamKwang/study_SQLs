0e9b9e77bd014e0da5b5bfda9e276305f44eca7e
Giljinsu <therin234@gmail.com>
2022-10-17 15:18:49 +0900

    22

--- jinsugil/w3schools/Categories_Products_totals.sql
+++ jinsugil/w3schools/Categories_Products_totals.sql
@@ -1,9 +1,9 @@
 --+5 : ? 분류(Categories)가 Produce, Beverages인 제품(Products) 총 갯수는 ?
 
-SELECT COUNT(CategoryID) 
+SELECT COUNT(ProductID) 
 FROM Products
 WHERE CategoryID IN(SELECT CategoryID
 					FROM Categories
                     WHERE CategoryName IN('Produce','Beverages'));
-
+                    
 --17개
