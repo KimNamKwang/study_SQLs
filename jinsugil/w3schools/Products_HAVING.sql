537c507638cbe1865aa7cb77e807531266fa3869
Giljinsu <therin234@gmail.com>
2022-10-19 09:54:47 +0900

    first

--- jinsugil/w3schools/Products_HAVING.sql
+++ jinsugil/w3schools/Products_HAVING.sql
@@ -1,6 +1,13 @@
+-- +1 카데고리에 제품이 10 개 이상인 카테고리 표시
 SELECT CategoryName
 FROM Categories
 WHERE CategoryID in (SELECT CategoryID
 					FROM Products
                     GROUP BY CategoryID
                     HAVING COUNT(*)>=10);
+
+-- Beverages
+-- Condiments
+-- Confections
+-- Dairy Products
+-- Seafood
