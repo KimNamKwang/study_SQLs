a389e1ec375b9e8c6a1eefd8ecf831f5e59d08a3
Giljinsu <therin234@gmail.com>
2022-10-20 13:46:13 +0900

    33

--- jinsugil/w3schools/Products_CategoryID.sql
+++ jinsugil/w3schools/Products_CategoryID.sql
@@ -1,12 +1,9 @@
 -- Proudcts에 CategoryID가 5,6을 표시, 내용 프로덕트 이름, 공급자 이름 출력?
 
 SELECT Products.ProductName, Suppliers.SupplierName
-FROM Products INNER JOIN Categories
-	ON Products.CategoryID = Categories.CategoryID	
-	AND Categories.CategoryID IN (5,6)
-    INNER JOIN Suppliers
+FROM Products INNER JOIN Suppliers
     	ON Products.SupplierID = Suppliers.SupplierID
-ORDER BY Categories.CategoryID
+WHERE Products.CategoryID IN (5,6)
 ;
 
 -- ProductName	SupplierName
